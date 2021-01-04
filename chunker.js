f8a4c35206032d1b803bc6faa5b1ac943df7eeb9
x <x>
2021-01-04 11:38:35 +0200

    path handling and some tweaks

--- chunker.js
+++ chunker.js
@@ -1,9 +1,10 @@
+import path from 'path';
 import fs from 'fs';
 const fsPromises = fs.promises;
 
 const maxChunkSize = 1024 * 1024 * 25; // 25MB
 
- // in real life I would use something like yargs to handle the command line arguments
+// in real life I would use something like yargs to handle command line arguments
 const args = process.argv.slice(2);
 const inputPath = args[1];
 const outputPath = args[2];
@@ -22,13 +23,13 @@ switch (args[0]) {
 
 
 // TODO: deal with relative and absolute pathes, escape double slashes
+// TODO: mkdir if output path doesnt exist
 
-
-const baseChunkName = "Chunk";
 function splitFileToChunks(inputFile, outputDir) {
 
     return new Promise((resolve, reject) => {
-        
+
+        let inputFileName = path.parse(inputFile).name;
         let readerStream = fs.createReadStream(inputFile, {highWaterMark: maxChunkSize});
         readerStream.on('error', (err) => reject(err));
         let metadata = {files: [], numberOfFiles: undefined}; // numberOfFiles can already be calculated, and array can be initalized with size
@@ -36,11 +37,11 @@ function splitFileToChunks(inputFile, outputDir) {
         let index = 0;
         readerStream.on('data', (chunk) => {
             console.log(chunk.length);
-            let outputFile = `${outputDir}/${baseChunkName}_${index}`;
+            let outputFile = path.normalize(`${outputDir}/${inputFileName}_${index}`);
             let i = index;
             let writeFilePromise = fsPromises.writeFile(outputFile, chunk)
-                .then(() => metadata.files[i] = outputFile) // see above; dont write full path 
-                .catch(() => {throw new Error('Cannot save chunk to file')});
+                .then(() => metadata.files[i] = outputFile)
+                .catch(() => reject(new Error('Cannot save chunk to file')));
             writeFilePromises[index] = writeFilePromise;
             index++;
         });
@@ -50,19 +51,19 @@ function splitFileToChunks(inputFile, outputDir) {
                 .then(() => fsPromises.writeFile(`${outputDir}/metadata.json`, JSON.stringify(metadata)))
                 .then(resolve, reject);
         });
-        
+
     });
 }
 
 function mergeChunksToFile(inputDir, outputFile) {
 
+    // TODO: this is possibly an anti-pattern
     return new Promise((resolve, reject) => {
 
         let filesPromise = fsPromises.readFile(`${inputDir}/metadata.json`)
             .then(JSON.parse)
             .then((metadata) => metadata.files);
         
-        // break the chain. would have been more elegant with async/await.
         // checking if files exists in parallel isn't really helpful but whatever
         let checkFilesPromise = filesPromise.then((files) => {
                 return files.map((file) => fsPromises.access(file));
@@ -73,9 +74,10 @@ function mergeChunksToFile(inputDir, outputFile) {
         let writerStream = fs.createWriteStream(outputFile);
         writerStream.on('error', (err) => reject(err));
 
+        // break the chain, so this happens only after we check that the files exist.
+        // would have been more elegant with async/await.
         Promise.all([filesPromise, checkFilesPromise])
             .then(([files,_]) => {
-                // TODO: handle errors
                 let p = Promise.resolve();
                 for (let i = 0; i < files.length; i++){
                     p = p.then(() => readAndPipePromise(files[i], writerStream));
@@ -91,11 +93,11 @@ function mergeChunksToFile(inputDir, outputFile) {
 
 /**
  * Take a file, create a read stream and pipe it to a given open write stream.
- * Returns a promise that will resolve when the reading ended.
+ * Returns a promise that will resolve when the reading ends.
  * 
  * @param {String} fileToRead A file to read from by creating a read stream.
  * @param {fs.WriteStream} [writeStream] A write stream into which we pipe. Must be already open and will remain open afterwards.
- * @returns {Promise} A promise that will be resolved when the reading ended.
+ * @returns {Promise} A promise that will be resolved when the reading ends.
  */
 function readAndPipePromise(fileToRead, writeStream) {
     return new Promise((resolve, reject) => {
